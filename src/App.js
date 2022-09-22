3dfc0a3959e2db3a1ec7f197cbd378920a851dcb
MD Tazbirul Haque <tazbirul94@gmail.com>
2022-09-22 19:01:43 +0200

    container removed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 import Content from './components/CustomNav';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
 
 function App() {
   // const [warning, setWarning] = React.useState(false);
   return (
-    <Container>
+    <div>
       <Header />
       <Content />
-    </Container>
+    </div>
   );
 }
 
