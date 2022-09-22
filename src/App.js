22d19df0381ff615420813353fd90a81f467721e
MD Tazbirul Haque <tazbirul94@gmail.com>
2022-09-22 19:01:10 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import Content from './components/CustomNav';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Container } from 'react-bootstrap';
 
 function App() {
   // const [warning, setWarning] = React.useState(false);
   return (
-    <div>
+    <Container>
       <Header />
       <Content />
-    </div>
+    </Container>
   );
 }
 
